adee3ed5ca7bbebb3a7d8f73c80a7a0add6bfb18
KevinVision <nickname@agi7.ai>
2025-01-20 19:02:28 +0800

    feat: encode one frame

--- encode.go
+++ encode.go
@@ -67,6 +67,36 @@ func NewEncoder(w io.Writer, opts *Options) (*Encoder, error) {
 	return e, nil
 }
 
+// NewEncoder returns new AAC encoder.
+func NewEncoderV2(opts *Options) (*Encoder, error) {
+	e := &Encoder{}
+	if opts.BitRate == 0 {
+		opts.BitRate = 64000
+	}
+
+	ret := aacenc.Init(aacenc.VoAudioCodingAac)
+	err := aacenc.ErrorFromResult(ret)
+	if err != nil {
+		return nil, fmt.Errorf("aac: %w", err)
+	}
+
+	var params aacenc.Param
+	params.SampleRate = int32(opts.SampleRate)
+	params.BitRate = int32(opts.BitRate)
+	params.NChannels = int16(opts.NumChannels)
+	params.AdtsUsed = 1
+
+	ret = aacenc.SetParam(aacenc.VoPidAacEncparam, unsafe.Pointer(&params))
+	err = aacenc.ErrorFromResult(ret)
+	if err != nil {
+		return nil, fmt.Errorf("aac: %w", err)
+	}
+
+	e.outbuf = make([]byte, 20480)
+
+	return e, nil
+}
+
 // Encode encodes data from reader.
 func (e *Encoder) Encode(r io.Reader) error {
 	inputEmpty := false
@@ -120,6 +150,45 @@ func (e *Encoder) Encode(r io.Reader) error {
 	}
 
 	return nil
+}
+
+// Encode encodes data from reader.
+func (e *Encoder) EncodeOneFrame(inbuf []byte) ([][]byte, error) {
+	var outinfo aacenc.VoAudioOutputinfo
+	var input, output aacenc.VoCodecBuffer
+
+	input.Buffer = C.CBytes(inbuf)
+	input.Length = uint64(len(inbuf))
+
+	ret := aacenc.SetInputData(&input)
+	err := aacenc.ErrorFromResult(ret)
+	if err != nil {
+		return nil, fmt.Errorf("aac: %w", err)
+	}
+
+	outputEmpty := false
+	var outDataList [][]byte
+	for !outputEmpty {
+		output.Buffer = C.CBytes(e.outbuf)
+		output.Length = uint64(len(e.outbuf))
+
+		ret = aacenc.GetOutputData(&output, &outinfo)
+		err = aacenc.ErrorFromResult(ret)
+		if err != nil {
+			if !errors.Is(err, aacenc.ErrInputBufferSmall) {
+				return nil, fmt.Errorf("aac: %w", err)
+			}
+			outputEmpty = true
+		}
+
+		outData := C.GoBytes(output.Buffer, C.int(output.Length))
+		outDataList = append(outDataList, outData)
+		C.free(output.Buffer)
+	}
+
+	C.free(input.Buffer)
+
+	return outDataList, nil
 }
 
 // Close closes encoder.
