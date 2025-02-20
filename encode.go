785f7e0bacd5c80fb055569019912e2f1ad1e0c3
KevinVision <nickname@agi7.ai>
2025-02-20 16:40:21 +0800

    fix: handle map

--- encode.go
+++ encode.go
@@ -11,7 +11,6 @@ import "C"
 import (
 	"errors"
 	"fmt"
-	"io"
 	"unsafe"
 
 	"github.com/gen2brain/aac-go/aacenc"
@@ -29,46 +28,7 @@ type Options struct {
 
 // Encoder type.
 type Encoder struct {
-	w io.Writer
-
-	insize int
-	inbuf  []byte
-	outbuf []byte
-}
-
-// NewEncoder returns new AAC encoder.
-func NewEncoder(w io.Writer, opts *Options) (*Encoder, error) {
-	e := &Encoder{}
-	e.w = w
-
-	if opts.BitRate == 0 {
-		opts.BitRate = 64000
-	}
-
-	ret := aacenc.Init(aacenc.VoAudioCodingAac)
-	err := aacenc.ErrorFromResult(ret)
-	if err != nil {
-		return nil, fmt.Errorf("aac: %w", err)
-	}
-
-	var params aacenc.Param
-	params.SampleRate = int32(opts.SampleRate)
-	params.BitRate = int32(opts.BitRate)
-	params.NChannels = int16(opts.NumChannels)
-	params.AdtsUsed = 1
-
-	ret = aacenc.SetParam(aacenc.VoPidAacEncparam, unsafe.Pointer(&params))
-	err = aacenc.ErrorFromResult(ret)
-	if err != nil {
-		return nil, fmt.Errorf("aac: %w", err)
-	}
-
-	e.insize = opts.NumChannels * 2 * 1024
-
-	e.inbuf = make([]byte, e.insize)
-	e.outbuf = make([]byte, 20480)
-
-	return e, nil
+	handleID int
 }
 
 // NewEncoder returns new AAC encoder.
@@ -78,7 +38,7 @@ func NewEncoderV2(opts *Options) (*Encoder, error) {
 		opts.BitRate = 64000
 	}
 
-	ret := aacenc.Init(aacenc.VoAudioCodingAac)
+	handleID, ret := aacenc.Init(aacenc.VoAudioCodingAac)
 	err := aacenc.ErrorFromResult(ret)
 	if err != nil {
 		return nil, fmt.Errorf("aac: %w", err)
@@ -90,68 +50,13 @@ func NewEncoderV2(opts *Options) (*Encoder, error) {
 	params.NChannels = int16(opts.NumChannels)
 	params.AdtsUsed = 1
 
-	ret = aacenc.SetParam(aacenc.VoPidAacEncparam, unsafe.Pointer(&params))
+	ret = aacenc.SetParam(handleID, aacenc.VoPidAacEncparam, unsafe.Pointer(&params))
 	err = aacenc.ErrorFromResult(ret)
 	if err != nil {
 		return nil, fmt.Errorf("aac: %w", err)
 	}
-
+	e.handleID = handleID
 	return e, nil
-}
-
-// Encode encodes data from reader.
-func (e *Encoder) Encode(r io.Reader) error {
-	inputEmpty := false
-	for !inputEmpty {
-		n, err := r.Read(e.inbuf)
-		if err != nil {
-			if !errors.Is(err, io.EOF) {
-				return fmt.Errorf("aac: %w", err)
-			}
-			inputEmpty = true
-		}
-
-		if n < e.insize {
-			inputEmpty = true
-		}
-
-		var outinfo aacenc.VoAudioOutputinfo
-		var input, output aacenc.VoCodecBuffer
-
-		input.Buffer = C.CBytes(e.inbuf)
-		input.Length = uint64(n)
-
-		ret := aacenc.SetInputData(&input)
-		err = aacenc.ErrorFromResult(ret)
-		if err != nil {
-			return fmt.Errorf("aac: %w", err)
-		}
-
-		outputEmpty := false
-		for !outputEmpty {
-			output.Buffer = C.CBytes(e.outbuf)
-			output.Length = uint64(len(e.outbuf))
-
-			ret = aacenc.GetOutputData(&output, &outinfo)
-			err = aacenc.ErrorFromResult(ret)
-			if err != nil {
-				if !errors.Is(err, aacenc.ErrInputBufferSmall) {
-					return fmt.Errorf("aac: %w", err)
-				}
-				outputEmpty = true
-			}
-
-			_, err = e.w.Write(C.GoBytes(output.Buffer, C.int(output.Length)))
-			if err != nil {
-				return fmt.Errorf("aac: %w", err)
-			}
-			C.free(output.Buffer)
-		}
-
-		C.free(input.Buffer)
-	}
-
-	return nil
 }
 
 // Encode encodes data from inbuf.
@@ -165,7 +70,7 @@ func (e *Encoder) EncodeOneFrame(inbuf []byte) ([][]byte, error) {
 	}
 	defer C.free(input.Buffer)
 	input.Length = uint64(len(inbuf))
-	ret := aacenc.SetInputData(&input)
+	ret := aacenc.SetInputData(e.handleID, &input)
 	err := aacenc.ErrorFromResult(ret)
 	if err != nil {
 		return nil, fmt.Errorf("aac: %w", err)
@@ -179,7 +84,7 @@ func (e *Encoder) EncodeOneFrame(inbuf []byte) ([][]byte, error) {
 	defer C.free(output.Buffer)
 	for {
 		output.Length = bufferSize
-		ret = aacenc.GetOutputData(&output, &outinfo)
+		ret = aacenc.GetOutputData(e.handleID, &output, &outinfo)
 		err = aacenc.ErrorFromResult(ret)
 		if err != nil {
 			if !errors.Is(err, aacenc.ErrInputBufferSmall) {
@@ -197,6 +102,6 @@ func (e *Encoder) EncodeOneFrame(inbuf []byte) ([][]byte, error) {
 
 // Close closes encoder.
 func (e *Encoder) Close() error {
-	ret := aacenc.Uninit()
+	ret := aacenc.Uninit(e.handleID)
 	return aacenc.ErrorFromResult(ret)
 }
